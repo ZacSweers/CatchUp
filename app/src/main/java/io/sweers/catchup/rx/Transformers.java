1048358c96ce4ea9aff2caeff6fb703c6664cfbf
Zac Sweers <pandanomic@gmail.com>
2001-01-01 03:25:00 +0100

    Normalizing operator!

--- app/src/main/java/io/sweers/catchup/rx/Transformers.java
+++ app/src/main/java/io/sweers/catchup/rx/Transformers.java
@@ -1,8 +1,11 @@
 package io.sweers.catchup.rx;
 
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.Observable.Transformer;
 import rx.functions.Action0;
+import rx.schedulers.Schedulers;
 
 public final class Transformers {
   private Transformers() {
@@ -12,5 +15,10 @@ public final class Transformers {
   public static <T> Transformer<T, T> doOnEmpty(Action0 action) {
     return source -> source
         .switchIfEmpty(Observable.<T>empty().doOnCompleted(action));
+  }
+
+  public static <T> Transformer<T, T> normalize(long time, TimeUnit unit) {
+    return source -> source
+        .lift(new OperatorNormalize<>(time, unit, Schedulers.computation()));
   }
 }
--- testing-utils/src/main/java/io/sweers/testutils/RecordingObserver.java
+++ testing-utils/src/main/java/io/sweers/testutils/RecordingObserver.java
@@ -1,6 +1,6 @@
 package io.sweers.testutils;
 
-import android.util.Log;
+import com.google.common.truth.Platform;
 
 import java.util.NoSuchElementException;
 import java.util.concurrent.BlockingDeque;
@@ -12,94 +12,104 @@ import rx.Observer;
 import static com.google.common.truth.Truth.assertThat;
 
 public final class RecordingObserver<T> implements Observer<T> {
-    private static final String TAG = "RecordingObserver";
+  private static final String TAG = "RecordingObserver";
 
-    private final BlockingDeque<Object> events = new LinkedBlockingDeque<>();
+  private final BlockingDeque<Object> events = new LinkedBlockingDeque<>();
 
-    @Override
-    public void onCompleted() {
-        Log.v(TAG, "onCompleted");
-        events.addLast(new OnCompleted());
+  @Override
+  public void onCompleted() {
+//    Log.v(TAG, "onCompleted");
+    events.addLast(new OnCompleted());
+  }
+
+  @Override
+  public void onError(Throwable e) {
+//    Log.v(TAG, "onError", e);
+    events.addLast(new OnError(e));
+  }
+
+  @Override
+  public void onNext(T t) {
+//    Log.v(TAG, "onNext " + t);
+    events.addLast(new OnNext(t));
+  }
+
+  private <E> E takeEvent(Class<E> wanted) {
+    Object event;
+    try {
+      event = events.pollFirst(1, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    if (event == null) {
+      throw new NoSuchElementException(
+          "No event found while waiting for " + wanted.getSimpleName());
+    }
+    assertThat(event).isInstanceOf(wanted);
+    return wanted.cast(event);
+  }
+
+  public boolean hasNextEvent() {
+    Object event;
+    try {
+      event = events.pollFirst(0, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    return event != null && Platform.isInstanceOfType(event, OnNext.class);
+  }
+
+  public T takeNext() {
+    OnNext event = takeEvent(OnNext.class);
+    return event.value;
+  }
+
+  public Throwable takeError() {
+    return takeEvent(OnError.class).throwable;
+  }
+
+  public void assertOnCompleted() {
+    takeEvent(OnCompleted.class);
+  }
+
+  public void assertNoMoreEvents() {
+    try {
+      Object event = takeEvent(Object.class);
+      throw new IllegalStateException("Expected no more events but got " + event);
+    } catch (NoSuchElementException ignored) {
+    }
+  }
+
+  private final class OnNext {
+    final T value;
+
+    private OnNext(T value) {
+      this.value = value;
     }
 
     @Override
-    public void onError(Throwable e) {
-        Log.v(TAG, "onError", e);
-        events.addLast(new OnError(e));
+    public String toString() {
+      return "OnNext[" + value + "]";
+    }
+  }
+
+  private final class OnCompleted {
+    @Override
+    public String toString() {
+      return "OnCompleted";
+    }
+  }
+
+  private final class OnError {
+    private final Throwable throwable;
+
+    private OnError(Throwable throwable) {
+      this.throwable = throwable;
     }
 
     @Override
-    public void onNext(T t) {
-        Log.v(TAG, "onNext " + t);
-        events.addLast(new OnNext(t));
+    public String toString() {
+      return "OnError[" + throwable + "]";
     }
-
-    private <E> E takeEvent(Class<E> wanted) {
-        Object event;
-        try {
-            event = events.pollFirst(1, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        if (event == null) {
-            throw new NoSuchElementException(
-                    "No event found while waiting for " + wanted.getSimpleName());
-        }
-        assertThat(event).isInstanceOf(wanted);
-        return wanted.cast(event);
-    }
-
-    public T takeNext() {
-        OnNext event = takeEvent(OnNext.class);
-        return event.value;
-    }
-
-    public Throwable takeError() {
-        return takeEvent(OnError.class).throwable;
-    }
-
-    public void assertOnCompleted() {
-        takeEvent(OnCompleted.class);
-    }
-
-    public void assertNoMoreEvents() {
-        try {
-            Object event = takeEvent(Object.class);
-            throw new IllegalStateException("Expected no more events but got " + event);
-        } catch (NoSuchElementException ignored) {
-        }
-    }
-
-    private final class OnNext {
-        final T value;
-
-        private OnNext(T value) {
-            this.value = value;
-        }
-
-        @Override
-        public String toString() {
-            return "OnNext[" + value + "]";
-        }
-    }
-
-    private final class OnCompleted {
-        @Override
-        public String toString() {
-            return "OnCompleted";
-        }
-    }
-
-    private final class OnError {
-        private final Throwable throwable;
-
-        private OnError(Throwable throwable) {
-            this.throwable = throwable;
-        }
-
-        @Override
-        public String toString() {
-            return "OnError[" + throwable + "]";
-        }
-    }
+  }
 }
