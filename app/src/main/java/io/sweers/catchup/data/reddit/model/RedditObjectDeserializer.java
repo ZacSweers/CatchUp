bec2a1a84b18b72b9d4aab2b52abeb18571d0965
Zac Sweers <pandanomic@gmail.com>
2016-05-03 23:58:17 +0200

    Use existing gson context

--- app/src/main/java/io/sweers/catchup/data/reddit/model/RedditObjectDeserializer.java
+++ app/src/main/java/io/sweers/catchup/data/reddit/model/RedditObjectDeserializer.java
@@ -2,7 +2,6 @@ package io.sweers.catchup.data.reddit.model;
 
 import android.util.Log;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -22,7 +21,7 @@ public class RedditObjectDeserializer implements JsonDeserializer<RedditObject> 
       return null;
     }
     try {
-      RedditObjectWrapper wrapper = new Gson().fromJson(json, RedditObjectWrapper.class);
+      RedditObjectWrapper wrapper = context.deserialize(json, RedditObjectWrapper.class);
       return context.deserialize(wrapper.getData(), wrapper.getKind().getDerivedClass());
     } catch (JsonParseException e) {
       Log.e(TAG, "Failed to deserialize", e);
