77c1a23c0cfcf80abf4d0ccc9f7061c086016f13
Zac Sweers <pandanomic@gmail.com>
2016-05-13 00:03:09 -0700

    Add delayedMessage
    Useful if you hve something that takes a long time to return but don't always want to immediately show a message (avoids flashing)

--- app/src/main/java/io/sweers/catchup/rx/Transformers.java
+++ app/src/main/java/io/sweers/catchup/rx/Transformers.java
@@ -1,11 +1,16 @@
 package io.sweers.catchup.rx;
 
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Observable.Transformer;
 import rx.functions.Action0;
 import rx.schedulers.Schedulers;
+
+import static rx.android.MainThreadSubscription.verifyMainThread;
 
 public final class Transformers {
   private Transformers() {
@@ -20,5 +25,30 @@ public final class Transformers {
   public static <T> Transformer<T, T> normalize(long time, TimeUnit unit) {
     return source -> source
         .lift(new OperatorNormalize<>(time, unit, Schedulers.computation()));
+  }
+
+  public static <T> Transformer<T, T> delayedMessage(View view, String message) {
+    return new Transformer<T, T>() {
+
+      private Observable<Long> timer = Observable.timer(300, TimeUnit.MILLISECONDS);
+      private Snackbar snackbar = null;
+
+      @Override
+      public Observable<T> call(Observable<T> source) {
+        verifyMainThread();
+        return source
+            .doOnSubscribe(() -> timer.takeUntil(source)
+                .subscribe(aLong -> {
+                  snackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE);
+                  snackbar.show();
+                }))
+            .doOnUnsubscribe(() -> {
+              if (snackbar != null) {
+                snackbar.dismiss();
+                snackbar = null;
+              }
+            });
+      }
+    };
   }
 }
