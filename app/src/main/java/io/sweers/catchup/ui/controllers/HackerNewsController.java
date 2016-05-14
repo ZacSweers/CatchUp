f70bd8b57724c308a8c5cc5a6cd2732a617e8002
Zac Sweers <pandanomic@gmail.com>
2016-05-13 19:55:34 -0700

    Remove HN tag because they're always Story

--- app/src/main/java/io/sweers/catchup/ui/controllers/HackerNewsController.java
+++ app/src/main/java/io/sweers/catchup/ui/controllers/HackerNewsController.java
@@ -23,7 +23,6 @@ import io.sweers.catchup.data.hackernews.HackerNewsService;
 import io.sweers.catchup.data.hackernews.model.HackerNewsStory;
 import io.sweers.catchup.injection.qualifiers.ForApi;
 import io.sweers.catchup.injection.scopes.PerController;
-import io.sweers.catchup.rx.Confine;
 import io.sweers.catchup.ui.activity.ActivityComponent;
 import io.sweers.catchup.ui.activity.MainActivity;
 import io.sweers.catchup.ui.base.BaseNewsController;
@@ -86,7 +85,7 @@ public final class HackerNewsController extends BaseNewsController<HackerNewsSto
       commentsCount = kids.size();
     }
     holder.comments(commentsCount);
-    holder.tag(story.type().tag());
+    holder.tag(null);
 
     holder.itemClicks()
         .compose(transformUrl(url))
