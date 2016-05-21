bca2106d95c7e8ada59b3867586cc779e0106290
Zac Sweers <pandanomic@gmail.com>
2016-05-21 15:49:19 -0700

    Statusbar coloring

--- app/src/main/java/io/sweers/catchup/ui/controllers/PagerController.java
+++ app/src/main/java/io/sweers/catchup/ui/controllers/PagerController.java
@@ -1,9 +1,13 @@
 package io.sweers.catchup.ui.controllers;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
@@ -12,10 +16,12 @@ import android.support.design.widget.TabLayout;
 import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import butterknife.BindView;
 import butterknife.Unbinder;
 import com.bluelinelabs.conductor.Controller;
@@ -51,20 +57,20 @@ public class PagerController extends ButterKnifeController {
       {
           R.drawable.logo_hn, R.string.hacker_news, R.color.hackerNewsAccent
       }, {
-          R.drawable.logo_reddit, R.string.reddit, R.color.redditAccent
-      }, {
-          R.drawable.logo_medium, R.string.medium, R.color.mediumAccent
-      }, {
-          R.drawable.logo_ph, R.string.product_hunt, R.color.productHuntAccent
-      }, {
-          R.drawable.logo_sd, R.string.slashdot, R.color.slashdotAccent
-      }, {
-          R.drawable.logo_dn, R.string.designer_news, R.color.designerNewsAccent
-      }, {
-          R.drawable.logo_dribbble, R.string.dribbble, R.color.dribbbleAccent
-      }, {
-          R.drawable.logo_github, R.string.github, R.color.githubAccent
-      }
+      R.drawable.logo_reddit, R.string.reddit, R.color.redditAccent
+  }, {
+      R.drawable.logo_medium, R.string.medium, R.color.mediumAccent
+  }, {
+      R.drawable.logo_ph, R.string.product_hunt, R.color.productHuntAccent
+  }, {
+      R.drawable.logo_sd, R.string.slashdot, R.color.slashdotAccent
+  }, {
+      R.drawable.logo_dn, R.string.designer_news, R.color.designerNewsAccent
+  }, {
+      R.drawable.logo_dribbble, R.string.dribbble, R.color.dribbbleAccent
+  }, {
+      R.drawable.logo_github, R.string.github, R.color.githubAccent
+  }
   };
   private final int[] resolvedColorCache = new int[PAGE_DATA.length];
   private final ArgbEvaluator argbEvaluator = new ArgbEvaluator();
@@ -138,6 +144,22 @@ public class PagerController extends ButterKnifeController {
     Arrays.fill(resolvedColorCache, R.color.no_color);
   }
 
+  public static void setLightStatusBar(@NonNull View view) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      int flags = view.getSystemUiVisibility();
+      flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+      view.setSystemUiVisibility(flags);
+    }
+  }
+
+  public static void clearLightStatusBar(@NonNull View view) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      int flags = view.getSystemUiVisibility();
+      flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+      view.setSystemUiVisibility(flags);
+    }
+  }
+
   @Override
   protected View inflateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
     return inflater.inflate(R.layout.controller_pager, container, false);
@@ -151,6 +173,40 @@ public class PagerController extends ButterKnifeController {
     super.onViewBound(view);
     ConductorInjection.inject(this);
 
+    @ColorInt int colorPrimaryDark =
+        UiUtil.resolveAttribute(view.getContext(), R.attr.colorPrimaryDark);
+    boolean isInNightMode = UiUtil.isInNightMode(view.getContext());
+    appBarLayout.addOnOffsetChangedListener((abl, verticalOffset) -> {
+      if (verticalOffset == -toolbar.getHeight()) {
+        int newStatusColor = getAndSaveColor(tabLayout.getSelectedTabPosition());
+        ValueAnimator statusBarColorAnim = ValueAnimator.ofArgb(colorPrimaryDark, newStatusColor);
+        statusBarColorAnim.addUpdateListener(animation -> getActivity().getWindow()
+            .setStatusBarColor((int) animation.getAnimatedValue()));
+        statusBarColorAnim.setDuration(200);
+        statusBarColorAnim.setInterpolator(new FastOutSlowInInterpolator());
+        statusBarColorAnim.start();
+        clearLightStatusBar(abl);
+      } else {
+        if (getActivity().getWindow()
+            .getStatusBarColor() == getAndSaveColor(tabLayout.getSelectedTabPosition())) {
+          ValueAnimator statusBarColorAnim = ValueAnimator.ofArgb(getActivity().getWindow()
+              .getStatusBarColor(), colorPrimaryDark);
+          statusBarColorAnim.addUpdateListener(animation -> getActivity().getWindow()
+              .setStatusBarColor((int) animation.getAnimatedValue()));
+          statusBarColorAnim.setDuration(200);
+          statusBarColorAnim.setInterpolator(new DecelerateInterpolator());
+          if (!isInNightMode) {
+            statusBarColorAnim.addListener(new AnimatorListenerAdapter() {
+              @Override public void onAnimationEnd(Animator animation) {
+                setLightStatusBar(abl);
+                animation.removeListener(this);
+              }
+            });
+          }
+          statusBarColorAnim.start();
+        }
+      }
+    });
     toolbar.inflateMenu(R.menu.main);
     toolbar.setOnMenuItemClickListener(item -> {
       switch (item.getItemId()) {
