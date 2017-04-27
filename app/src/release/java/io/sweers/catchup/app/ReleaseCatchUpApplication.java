376e835b215cbdd6ddaf5386fbd285456645b74e
Zac Sweers <pandanomic@gmail.com>
2017-04-27 01:32:06 -0700

    Bugsnag config

--- app/src/release/java/io/sweers/catchup/app/ReleaseCatchUpApplication.java
+++ app/src/release/java/io/sweers/catchup/app/ReleaseCatchUpApplication.java
@@ -1,18 +1,22 @@
 package io.sweers.catchup.app;
 
+import com.bugsnag.android.Bugsnag;
 import com.squareup.leakcanary.RefWatcher;
+import io.sweers.catchup.BuildConfig;
+import timber.log.Timber;
 
 public final class ReleaseCatchUpApplication extends CatchUpApplication {
   @Override protected void initVariant() {
     refWatcher = RefWatcher.DISABLED;
-//    Bugsnag.init(this, "");
+    Bugsnag.init(this, BuildConfig.BUGSNAG_KEY);
 
-//    final BugsnagTree tree = new BugsnagTree();
-//    Bugsnag.getClient().beforeNotify(error -> {
-//      tree.update(error);
-//      return true;
-//    });
-//
-//    Timber.plant(tree);
+    final BugsnagTree tree = new BugsnagTree();
+    Bugsnag.getClient()
+        .beforeNotify(error -> {
+          tree.update(error);
+          return true;
+        });
+
+    Timber.plant(tree);
   }
 }
