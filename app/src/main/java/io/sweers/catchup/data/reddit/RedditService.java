6208f18bb703affe513b724698770339d91e78dd
Zac Sweers <pandanomic@gmail.com>
2017-04-29 17:11:57 -0700

    Fix return types for RedditService
    these were still RxJava 1

--- app/src/main/java/io/sweers/catchup/data/reddit/RedditService.java
+++ app/src/main/java/io/sweers/catchup/data/reddit/RedditService.java
@@ -23,22 +23,21 @@ import java.util.List;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
-import rx.Observable;
 
 public interface RedditService {
   String HOST = "www.reddit.com";
   String ENDPOINT = "https://" + HOST;
 
-  @GET("/r/{subreddit}/comments/{id}") Observable<List<RedditResponse>> comments(
+  @GET("/r/{subreddit}/comments/{id}") Single<List<RedditResponse>> comments(
       @Path("subreddit") String subreddit, @Path("id") String id);
 
   @GET("/") Single<RedditResponse> frontPage(@Query("limit") int limit,
       @Query("after") @Nullable String after);
 
-  @GET("/r/{subreddit}") Observable<RedditResponse> subreddit(@Path("subreddit") String subreddit,
+  @GET("/r/{subreddit}") Single<RedditResponse> subreddit(@Path("subreddit") String subreddit,
       @Query("after") String after,
       @Query("limit") int limit);
 
-  @GET("/top") Observable<RedditResponse> top(@Query("after") String after,
+  @GET("/top") Single<RedditResponse> top(@Query("after") String after,
       @Query("limit") int limit);
 }
