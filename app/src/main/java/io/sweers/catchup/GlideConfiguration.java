04217e81c8f24604ce8feffaab20e40dae11f9c9
Zac Sweers <pandanomic@gmail.com>
2017-05-01 11:14:16 -0700

    Reformat GlideConf

--- app/src/main/java/io/sweers/catchup/GlideConfiguration.java
+++ app/src/main/java/io/sweers/catchup/GlideConfiguration.java
@@ -19,7 +19,6 @@ package io.sweers.catchup;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.support.v4.app.ActivityManagerCompat;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.load.DecodeFormat;
@@ -30,18 +29,15 @@ import com.bumptech.glide.module.GlideModule;
  */
 public final class GlideConfiguration implements GlideModule {
 
-  @Override
-  public void applyOptions(Context context, GlideBuilder builder) {
+  @Override public void applyOptions(Context context, GlideBuilder builder) {
     // Prefer higher quality images unless we're on a low RAM device
-    ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    ActivityManager activityManager =
+        (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
     builder.setDecodeFormat(
-        ActivityManagerCompat.isLowRamDevice(activityManager)
-            ? DecodeFormat.PREFER_RGB_565
-            : DecodeFormat.PREFER_ARGB_8888
-    );
+        ActivityManagerCompat.isLowRamDevice(activityManager) ? DecodeFormat.PREFER_RGB_565
+            : DecodeFormat.PREFER_ARGB_8888);
   }
 
-  @Override
-  public void registerComponents(Context context, Glide glide) {
+  @Override public void registerComponents(Context context, Glide glide) {
   }
 }
