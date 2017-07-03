05b629e8ab900146e3d53764c8e4d723fd2aacd2
Zac Sweers <pandanomic@gmail.com>
2017-07-02 19:05:37 -0700

    Remove java helpers

--- app/src/main/java/io/sweers/catchup/util/TimberExt.kt
+++ app/src/main/java/io/sweers/catchup/util/TimberExt.kt
@@ -38,9 +38,6 @@ inline fun e(throwable: Throwable, message: () -> String) = ifPlanted {
   Timber.e(throwable, message())
 }
 
-/** Delegates the throwable to [Timber.e]. Made for convenience when called from java */
-fun e(throwable: Throwable) = Timber.e(throwable)
-
 /** Delegates the provided message to [Timber.w] if any trees are planted. */
 inline fun w(message: () -> String) = ifPlanted { Timber.w(message()) }
 
@@ -48,9 +45,6 @@ inline fun w(message: () -> String) = ifPlanted { Timber.w(message()) }
 inline fun w(throwable: Throwable, message: () -> String) = ifPlanted {
   Timber.w(throwable, message())
 }
-
-/** Delegates the throwable to [Timber.w]. Made for convenience when called from java */
-fun w(throwable: Throwable) = Timber.w(throwable)
 
 /** Delegates the provided message to [Timber.i] if any trees are planted. */
 inline fun i(message: () -> String) = ifPlanted { Timber.i(message()) }
@@ -60,9 +54,6 @@ inline fun i(throwable: Throwable, message: () -> String) = ifPlanted {
   Timber.i(throwable, message())
 }
 
-/** Delegates the throwable to [Timber.i]. Made for convenience when called from java */
-fun i(throwable: Throwable) = Timber.i(throwable)
-
 /** Delegates the provided message to [Timber.d] if any trees are planted. */
 inline fun d(message: () -> String) = ifPlanted { Timber.d(message()) }
 
@@ -70,9 +61,6 @@ inline fun d(message: () -> String) = ifPlanted { Timber.d(message()) }
 inline fun d(throwable: Throwable, message: () -> String) = ifPlanted {
   Timber.d(throwable, message())
 }
-
-/** Delegates the throwable to [Timber.d]. Made for convenience when called from java */
-fun d(throwable: Throwable) = Timber.d(throwable)
 
 /** Delegates the provided message to [Timber.v] if any trees are planted. */
 inline fun v(message: () -> String) = ifPlanted { Timber.v(message()) }
@@ -82,9 +70,6 @@ inline fun v(throwable: Throwable, message: () -> String) = ifPlanted {
   Timber.v(throwable, message())
 }
 
-/** Delegates the throwable to [Timber.v]. Made for convenience when called from java */
-fun v(throwable: Throwable) = Timber.v(throwable)
-
 /** Delegates the provided message to [Timber.wtf] if any trees are planted. */
 inline fun wtf(message: () -> String) = ifPlanted { Timber.wtf(message()) }
 
@@ -92,9 +77,6 @@ inline fun wtf(message: () -> String) = ifPlanted { Timber.wtf(message()) }
 inline fun wtf(throwable: Throwable, message: () -> String) = ifPlanted {
   Timber.wtf(throwable, message())
 }
-
-/** Delegates the throwable to [Timber.wtf]. Made for convenience when called from java */
-fun wtf(throwable: Throwable) = Timber.wtf(throwable)
 
 /** Delegates the provided message to [Timber.log] if any trees are planted. */
 inline fun log(priority: Int, t: Throwable, message: () -> String) = ifPlanted {
@@ -104,5 +86,3 @@ inline fun log(priority: Int, t: Throwable, message: () -> String) = ifPlanted {
 /** Delegates the provided message to [Timber.log] if any trees are planted. */
 inline fun log(priority: Int, message: () -> String) = ifPlanted { Timber.log(priority, message()) }
 
-/** Delegates the throwable to [Timber.log]. Made for convenience when called from java */
-fun log(priority: Int, throwable: Throwable) = Timber.log(priority, throwable)
