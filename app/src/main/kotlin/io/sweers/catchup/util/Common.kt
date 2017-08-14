a54ce08726fb926f143a14c15fc39c8a8d937eb3
Zac Sweers <pandanomic@gmail.com>
2017-08-14 04:01:08 -0700

    Fix applyOn not being terminal

--- app/src/main/kotlin/io/sweers/catchup/util/Common.kt
+++ app/src/main/kotlin/io/sweers/catchup/util/Common.kt
@@ -4,5 +4,5 @@ package io.sweers.catchup.util
  * Applies a [block] on a set of [args].
  */
 inline fun <T> applyOn(vararg args: T, crossinline block: T.() -> Unit) {
-  args.asSequence().onEach { block(it) }
+  args.asSequence().forEach { block(it) }
 }
