748f11a18ea907a540343e44ce1452fb7d39c98f
Zac Sweers <zac.sweers@gmail.com>
2017-10-29 02:59:27 +0000

    Add comment linking issue tracker

--- util/src/main/kotlin/io/sweers/catchup/util/SdkVersionsExt.kt
+++ util/src/main/kotlin/io/sweers/catchup/util/SdkVersionsExt.kt
@@ -23,6 +23,8 @@ fun isN(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
 fun isO(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 fun isOMR1(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
 
+// Not totally safe to use yet
+// https://issuetracker.google.com/issues/64550633
 inline fun sdk(level: Int, func: () -> Unit) {
   if (Build.VERSION.SDK_INT >= level) func.invoke()
 }
