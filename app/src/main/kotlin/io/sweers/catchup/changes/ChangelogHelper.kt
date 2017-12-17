09afabc6922cbbb86cfb1b4b1af48c3172b0bfba
Zac Sweers <zac.sweers@gmail.com>
2017-12-16 21:54:09 -0800

    Don't show a changelog EDU on first open

--- app/src/main/kotlin/io/sweers/catchup/changes/ChangelogHelper.kt
+++ app/src/main/kotlin/io/sweers/catchup/changes/ChangelogHelper.kt
@@ -43,10 +43,15 @@ class ChangelogHelper @Inject constructor(
 
   fun bindWith(toolbar: Toolbar, @ColorInt hintColor: Int, linkColor: () -> Int) {
     val changelog = toolbar.resources.getString(R.string.changelog_text)
+    val lastVersion = sharedPreferences.getString("last_version", null)
     // Check if version name changed and if there's a changelog
-    if (sharedPreferences.getString("last_version", null) != BuildConfig.VERSION_NAME) {
+    if (lastVersion != BuildConfig.VERSION_NAME) {
       // Write the new version in
       sharedPreferences.edit().putString("last_version", BuildConfig.VERSION_NAME).apply()
+      if (lastVersion == null) {
+        // This was the first load it seems, so ignore it
+        return
+      }
       changelog.ifNotEmpty {
         toolbar.inflateMenu(R.menu.changes)
         with(toolbar.menu) {
