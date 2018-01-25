cd4fc64a2a8040143cf8c7cd7f4289a0d11a34bf
Zac Sweers <zac.sweers@gmail.com>
2018-01-24 23:17:16 -0800

    Add temporary sample data
    I can't get the integration module to work in android studio

---
+++ services/reddit/src/main/kotlin/io/sweers/catchup/service/reddit/model/SampleData.kt
@@ -0,0 +1,18 @@
+package io.sweers.catchup.service.reddit.model
+
+import com.squareup.moshi.Json
+import io.sweers.moshkt.api.MoshiSerializable
+
+@MoshiSerializable
+data class Foo(
+    @Json(name = "first_name") val firstName: String,
+    @Json(name = "last_name") val lastName: String,
+    val age: Int,
+    val nationalities: List<String> = emptyList(),
+    val weight: Float,
+    val tattoos: Boolean = false,
+    val race: String?,
+    val hasChildren: Boolean = false,
+    val favoriteFood: String? = null,
+    val favoriteDrink: String? = "Water"
+)
