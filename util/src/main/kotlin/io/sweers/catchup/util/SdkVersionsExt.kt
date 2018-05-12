8edc8f5a1329f295e6db655951b84112e9aea424
Zac Sweers <zac.sweers@gmail.com>
2018-05-12 15:55:05 -0700

    Add P sdk

--- util/src/main/kotlin/io/sweers/catchup/util/SdkVersionsExt.kt
+++ util/src/main/kotlin/io/sweers/catchup/util/SdkVersionsExt.kt
@@ -22,6 +22,7 @@ fun isM(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
 fun isN(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
 fun isO(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 fun isOMR1(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
+fun isP(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
 
 // Not totally safe to use yet
 // https://issuetracker.google.com/issues/64550633
