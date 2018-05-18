baaffdec8b14be4c2806a664b38cdc1a4195a1f0
Zac Sweers <zac.sweers@gmail.com>
2018-05-18 00:29:42 -0700

    Exclude more from the apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,22 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * Copyright (c) 2017 Zac Sweers
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import de.triplet.gradle.play.PlayAccountConfig
 import org.gradle.api.internal.plugins.DefaultConvention
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -98,6 +82,11 @@ android {
     exclude("META-INF/NOTICE.txt")
     exclude("META-INF/LICENSE.txt")
     exclude("META-INF/services/javax.annotation.processing.Processor")
+    exclude("kotlin/**")
+    exclude("**/*.kotlin_metadata")
+    exclude("META-INF/*.kotlin_module")
+    exclude("META-INF/*.version")
+    exclude("META-INF/com.uber.crumb/**")
   }
   buildTypes {
     getByName("debug") {
