79b657d59f8cc4b687052b7e9ded9ebfab263b65
Zac Sweers <zac.sweers@gmail.com>
2018-06-02 22:59:14 -0700

    agp 3.2-alpha16

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -99,7 +99,7 @@ object deps {
       const val perf = "com.google.firebase:firebase-perf:${versions.firebase}"
     }
 
-    const val gradlePlugin = "com.android.tools.build:gradle:3.2.0-alpha15"
+    const val gradlePlugin = "com.android.tools.build:gradle:3.2.0-alpha16"
     const val ktx = "androidx.core:core-ktx:1.0.0-alpha1"
 
     object support {
