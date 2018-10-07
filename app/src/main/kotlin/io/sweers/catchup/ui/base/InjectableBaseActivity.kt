054e8055214a96ed9a31968da24990c858f96a60
Zac Sweers <zac.sweers@gmail.com>
2018-10-07 16:03:38 +0200

    Add injectable bases

---
+++ app/src/main/kotlin/io/sweers/catchup/ui/base/InjectableBaseActivity.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Zac Sweers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.sweers.catchup.ui.base
+
+import android.os.Bundle
+import dagger.android.AndroidInjection
+
+abstract class InjectableBaseActivity : BaseActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    AndroidInjection.inject(this)
+    super.onCreate(savedInstanceState)
+  }
+
+}
