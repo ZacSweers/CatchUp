f327b25b6cb1fcd440b6e65ac5cf9eebbca20559
Zac Sweers <zac.sweers@gmail.com>
2018-10-17 16:37:09 -0400

    Flick 1.4.0

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -322,7 +322,7 @@ object deps {
       const val telescope = "com.mattprecious.telescope:telescope:2.1.0"
     }
 
-    const val flick = "me.saket:flick:1.3.0"
+    const val flick = "me.saket:flick:1.4.0"
     const val gestureViews = "com.alexvasilkov:gesture-views:2.2.0"
     const val inboxRecyclerView = "me.saket:inboxrecyclerview:1.0.0-SNAPSHOT"
     const val javaxInject = "org.glassfish:javax.annotation:10.0-b28"
