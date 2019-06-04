10b18de8e1adcc7b81f78b289e1b7dc3a74595db
Zac Sweers <zac.sweers@gmail.com>
2019-06-04 00:30:50 -0500

    Fix lint wiring (#155)
    Uses checkDependencies and (best effort) disables lintin in non-app modules

--- services/unsplash/build.gradle.kts
+++ services/unsplash/build.gradle.kts
@@ -42,18 +42,8 @@ android {
     targetCompatibility = JavaVersion.VERSION_1_8
   }
   lintOptions {
-    setLintConfig(file("lint.xml"))
-    isAbortOnError = true
-    check("InlinedApi")
-    check("NewApi")
-    fatal("NewApi")
-    fatal("InlinedApi")
-    enable("UnusedResources")
-    isCheckReleaseBuilds = true
-    textReport = deps.build.ci
-    textOutput("stdout")
-    htmlReport = !deps.build.ci
-    xmlReport = !deps.build.ci
+    isCheckReleaseBuilds = false
+    isAbortOnError = false
   }
   libraryVariants.all {
     generateBuildConfigProvider?.configure {
