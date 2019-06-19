f47a2846d0cdaa282f66a9571bdc1bcc84eeb341
Jared Burrows <jaredsburrows@gmail.com>
2019-06-19 19:58:24 -0400

    Remove 'buildToolsVersion' (#161)

--- services/medium/build.gradle.kts
+++ services/medium/build.gradle.kts
@@ -28,7 +28,6 @@ apply {
 
 android {
   compileSdkVersion(deps.android.build.compileSdkVersion)
-  buildToolsVersion(deps.android.build.buildToolsVersion)
 
   defaultConfig {
     minSdkVersion(deps.android.build.minSdkVersion)
