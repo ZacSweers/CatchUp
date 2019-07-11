8e24d6ee978d39db3a811743b20691aa2102186c
Zac Sweers <zac.sweers@gmail.com>
2019-07-10 19:14:21 -0500

    Remove temporary repro

--- services/github/src/main/kotlin/io/sweers/catchup/service/github/GitHubService.kt
+++ services/github/src/main/kotlin/io/sweers/catchup/service/github/GitHubService.kt
@@ -84,8 +84,6 @@ internal class GitHubService @Inject constructor(
   }
 
   private fun fetchByScraping(): Single<DataResult> {
-    // TODO temporary to repro the current bug
-    emojiMarkdownConverter.get()
     return gitHubApi
         .get()
         .getTrending(language = All, since = DAILY)
