837fdf997709df6ee8a8d4204fed2cce59ab6fe3
Zac Sweers <zac.sweers@gmail.com>
2019-08-13 17:51:11 -0400

    AGP 3.6-alpha-06

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -142,7 +142,7 @@ object deps {
       const val perf = "com.google.firebase:firebase-perf:18.0.0"
     }
 
-    const val gradlePlugin = "com.android.tools.build:gradle:3.6.0-alpha05"
+    const val gradlePlugin = "com.android.tools.build:gradle:3.6.0-alpha06"
   }
 
   object apollo {
