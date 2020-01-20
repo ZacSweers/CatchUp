e53dc3ed2f51513b798a607cc0a555cbc48cfb89
Zac Sweers <zsweers@slack-corp.com>
2020-01-19 22:56:45 -0500

    AGP alpha08

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -156,7 +156,7 @@ object deps {
       const val perf = "com.google.firebase:firebase-perf:18.0.0"
     }
 
-    const val gradlePlugin = "com.android.tools.build:gradle:4.0.0-alpha07"
+    const val gradlePlugin = "com.android.tools.build:gradle:4.0.0-alpha08"
   }
 
   object apollo {
