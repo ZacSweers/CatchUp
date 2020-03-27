3b7469fdbb50581dffa696caf44a5440eeabe86f
Zac Sweers <zsweers@slack-corp.com>
2020-03-27 19:11:35 -0400

    Imports

--- services/hackernews/src/main/kotlin/io/sweers/catchup/service/hackernews/HackerNewsCommentsFragment.kt
+++ services/hackernews/src/main/kotlin/io/sweers/catchup/service/hackernews/HackerNewsCommentsFragment.kt
@@ -20,9 +20,7 @@ import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
