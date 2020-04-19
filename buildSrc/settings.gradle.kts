e2fda0053909339c7ff641ec121aa8ebd90f7a29
Zac Sweers <zsweers@slack-corp.com>
2020-04-19 18:49:08 -0400

    AGP 4.1a6 and use kotlin 1.4-M1 in buildSrc

---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+  repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+    maven {
+      setUrl("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+    jcenter()
+  }
+}
