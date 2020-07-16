897101469b43a812cee0efa27ca2c3c0c85b0ed8
Zac Sweers <pandanomic@gmail.com>
2020-07-15 20:54:45 -0400

    AGP 4.2.0-alpha04

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -23,7 +23,7 @@ kotlinDslPluginOptions {
  * These are copied as a source into the main source set and templated for replacement.
  */
 object SharedBuildVersions {
-  const val agp = "4.2.0-alpha03"
+  const val agp = "4.2.0-alpha04"
   const val kotlin = "1.4-M3"
   const val moshi = "1.9.3"
   const val okio = "2.7.0"
