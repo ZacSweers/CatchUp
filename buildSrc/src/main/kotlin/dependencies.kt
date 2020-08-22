a73c54408f4a113d00c518a4d0ac8c5d9365693d
Zac Sweers <pandanomic@gmail.com>
2020-08-22 02:02:57 -0400

    Go to compile/target SDK 30 too

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -133,9 +133,9 @@ object deps {
     }
 
     object build {
-      const val compileSdkVersion = 29
+      const val compileSdkVersion = 30
       const val minSdkVersion = 21
-      const val targetSdkVersion = 29
+      const val targetSdkVersion = 30
     }
 
     object firebase {
