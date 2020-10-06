72a2a45650b1fc34755fa45f3b9c6a6e849a1bab
Nelson Osacky <nosacky@gradle.com>
2020-10-06 18:42:09 +0200

    Bump Gradle Doctor with negative avoidance savings threshold

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ buildscript {
 plugins {
   id("com.github.ben-manes.versions") version "0.33.0"
   id("catchup")
-  id("com.osacky.doctor") version "0.6.1"
+  id("com.osacky.doctor") version "0.6.2"
 }
 
 apply {
