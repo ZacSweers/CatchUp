cb6a6b560b545badd00d9a1d960593222938490f
Zac Sweers <pandanomic@gmail.com>
2021-12-17 17:17:43 -0500

    Coroutines 1.6.0-RC3

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -318,7 +318,7 @@ object deps {
   }
 
   object kotlin {
-    private const val coroutinesVersion = "1.5.2"
+    private const val coroutinesVersion = "1.6.0-RC3"
     const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
     const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
     const val coroutinesRx = "org.jetbrains.kotlinx:kotlinx-coroutines-rx3:$coroutinesVersion"
