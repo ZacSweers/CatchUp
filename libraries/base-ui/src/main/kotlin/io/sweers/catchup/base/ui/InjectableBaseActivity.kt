4f660dc8b9d5d13c2e9978d2546af1ecfb8df254
Zac Sweers <pandanomic@gmail.com>
2022-11-25 00:19:58 -0500

    More VM infra

--- libraries/base-ui/src/main/kotlin/io/sweers/catchup/base/ui/InjectableBaseActivity.kt
+++ libraries/base-ui/src/main/kotlin/io/sweers/catchup/base/ui/InjectableBaseActivity.kt
@@ -20,6 +20,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentManager.FragmentLifecycleCallbacks
+import androidx.lifecycle.ViewModelProvider
 import dev.zacsweers.catchup.appconfig.AppConfig
 import javax.inject.Inject
 
@@ -30,6 +31,7 @@ abstract class InjectableBaseActivity : BaseActivity() {
   }
 
   @Inject lateinit var viewContainer: ViewContainer
+  @Inject lateinit var viewModelProviderFactory: ViewModelProvider.Factory
   @Inject lateinit var uiPreferences: UiPreferences
   @Inject override lateinit var appConfig: AppConfig
   var fragmentFactory: FragmentFactory? = null
@@ -52,8 +54,8 @@ abstract class InjectableBaseActivity : BaseActivity() {
       supportFragmentManager.registerFragmentLifecycleCallbacks(callbacks, true)
     }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+  override fun getDefaultViewModelProviderFactory(): ViewModelProvider.Factory {
+    return viewModelProviderFactory
   }
 
   override fun getSystemServiceName(serviceClass: Class<*>): String? {
