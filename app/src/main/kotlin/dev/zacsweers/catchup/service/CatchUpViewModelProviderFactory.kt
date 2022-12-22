163b1e3471fa5ea5c968a82a5b05dddb999f934f
Zac Sweers <pandanomic@gmail.com>
2022-12-22 18:44:20 -0500

    Fix multibindings

--- app/src/main/kotlin/dev/zacsweers/catchup/service/CatchUpViewModelProviderFactory.kt
+++ app/src/main/kotlin/dev/zacsweers/catchup/service/CatchUpViewModelProviderFactory.kt
@@ -11,14 +11,13 @@ import dagger.multibindings.Multibinds
 import dev.zacsweers.catchup.di.AppScope
 import io.sweers.catchup.base.ui.ViewModelAssistedFactory
 import javax.inject.Inject
+import javax.inject.Provider
 
 @ContributesTo(AppScope::class)
 @Module
 abstract class VmMultibindings {
-  // TODO why does Dagger think this stub doesn't return a map...?
-  //  @Multibinds
-  //  abstract fun viewModelFactories():
-  //    Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<@JvmSuppressWildcards ViewModel>>
+  @Multibinds
+  abstract fun viewModelFactories(): Map<Class<out ViewModel>, @JvmSuppressWildcards ViewModel>
 
   @Multibinds
   abstract fun assistedViewModelFactories():
@@ -32,20 +31,18 @@ class CatchUpViewModelProviderFactory
 constructor(
   private val assistedProviders:
     Map<Class<out ViewModel>, @JvmSuppressWildcards ViewModelAssistedFactory<out ViewModel>>,
-//  private val modelProviders: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+  private val modelProviders: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
-  //  override fun <T : ViewModel> create(modelClass: Class<T>): T {
-  //    val modelProvider =
-  //      modelProviders[modelClass]
-  //        ?: throw IllegalArgumentException("Unknown ViewModel class: $modelClass")
-  //    @Suppress("UNCHECKED_CAST") return modelProvider.get() as T
-  //  }
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    val modelProvider =
+      modelProviders[modelClass]
+        ?: throw IllegalArgumentException("Unknown ViewModel class: $modelClass")
+    @Suppress("UNCHECKED_CAST") return modelProvider.get() as T
+  }
 
   override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-    val modelProvider =
-      assistedProviders[modelClass]
-        ?: throw IllegalArgumentException("Unknown ViewModel class: $modelClass")
+    val modelProvider = assistedProviders[modelClass] ?: return create(modelClass)
     @Suppress("UNCHECKED_CAST") return modelProvider.create(extras.createSavedStateHandle()) as T
   }
 }
