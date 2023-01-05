dea4153bc308140b2ae4277c2e9da946b73f2623
Zac Sweers <pandanomic@gmail.com>
2023-01-05 13:00:50 -0400

    Update proguard rules

--- libraries/retrofitconverters/src/main/kotlin/io/sweers/catchup/libraries/retrofitconverters/RetrofitExt.kt
+++ libraries/retrofitconverters/src/main/kotlin/io/sweers/catchup/libraries/retrofitconverters/RetrofitExt.kt
@@ -15,20 +15,10 @@
  */
 package io.sweers.catchup.libraries.retrofitconverters
 
-import okhttp3.Call
 import okhttp3.OkHttpClient
-import okhttp3.Request
 import retrofit2.Retrofit
 
-@PublishedApi
-internal inline fun Retrofit.Builder.callFactory(crossinline body: (Request) -> Call) =
-  callFactory(
-    object : Call.Factory {
-      override fun newCall(request: Request): Call = body(request)
-    }
-  )
-
-@Suppress("NOTHING_TO_INLINE")
-inline fun Retrofit.Builder.delegatingCallFactory(
-  delegate: dagger.Lazy<OkHttpClient>
-): Retrofit.Builder = callFactory { delegate.get().newCall(it) }
+fun Retrofit.Builder.delegatingCallFactory(delegate: dagger.Lazy<OkHttpClient>): Retrofit.Builder =
+  callFactory {
+    delegate.get().newCall(it)
+  }
