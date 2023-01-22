3dc4a3794f8ef0d1d656c80a24fc203d21e1c8dd
Zac Sweers <pandanomic@gmail.com>
2023-01-21 22:36:27 -0500

    Support choosers in IntentScreen

--- app/src/main/kotlin/dev/zacsweers/catchup/circuit/IntentScreen.kt
+++ app/src/main/kotlin/dev/zacsweers/catchup/circuit/IntentScreen.kt
@@ -2,18 +2,26 @@ package dev.zacsweers.catchup.circuit
 
 import android.app.Activity
 import android.content.Intent
+import androidx.compose.runtime.Stable
 import com.slack.circuit.Navigator
 import com.slack.circuit.Screen
+import io.sweers.catchup.util.maybeStartChooser
 import kotlinx.parcelize.Parcelize
 
-@Parcelize data class IntentScreen(val intent: Intent) : Screen
+@Parcelize data class IntentScreen(val intent: Intent, val isChooser: Boolean = false) : Screen
 
+@Stable
 class IntentAwareNavigator(private val activity: Activity, private val delegate: Navigator) :
   Navigator by delegate {
   override fun goTo(screen: Screen) {
     when (screen) {
       is IntentScreen -> {
-        activity.startActivity(screen.intent)
+        val intent = screen.intent
+        if (screen.isChooser) {
+          activity.maybeStartChooser(intent)
+        } else {
+          activity.startActivity(intent)
+        }
       }
       else -> delegate.goTo(screen)
     }
