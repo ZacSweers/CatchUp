a8c1d426c235d399bdbd2b2f6e1378f041a47a60
Zac Sweers <pandanomic@gmail.com>
2023-01-21 19:42:21 -0500

    Finish handling system bars in DebugRootContent

--- app/src/debug/kotlin/io/sweers/catchup/ui/debug/DebugRootContent.kt
+++ app/src/debug/kotlin/io/sweers/catchup/ui/debug/DebugRootContent.kt
@@ -7,22 +7,34 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.slack.circuit.CircuitContent
 import com.squareup.anvil.annotations.ContributesBinding
 import dev.zacsweers.catchup.di.AppScope
 import dev.zacsweers.catchup.di.SingleIn
 import io.sweers.catchup.base.ui.DefaultRootContent
 import io.sweers.catchup.base.ui.RootContent
+import io.sweers.catchup.data.DebugPreferences
 import javax.inject.Inject
 
 @SingleIn(AppScope::class)
 @ContributesBinding(AppScope::class, replaces = [DefaultRootContent::class])
-class DebugRootContent @Inject constructor() : RootContent {
+class DebugRootContent
+@Inject
+constructor(
+  private val debugPreferences: DebugPreferences,
+) : RootContent {
   @Composable
   override fun Content(content: @Composable () -> Unit) {
-    // TODO show w/ syllabus if not seen before
     val original = LocalLayoutDirection.current
     // This is how to get the drawer on the right and... guh
     val inverted =
@@ -32,10 +44,43 @@ class DebugRootContent @Inject constructor() : RootContent {
         LayoutDirection.Ltr
       }
     CompositionLocalProvider(LocalLayoutDirection provides inverted) {
-      val drawerState = rememberDrawerState(DrawerValue.Open)
-      // TODO set light icons when drawer is open
-      //  val systemUiController = rememberSystemUiController()
-      //  systemUiController.systemBarsDarkContentEnabled = drawerState.isOpen
+      val drawerState = rememberDrawerState(DrawerValue.Closed)
+      // TODO show w/ syllabus if not seen before
+      LaunchedEffect(drawerState) {
+        // Wait until fully drawn
+        withFrameNanos {}
+        debugPreferences.seenDebugDrawer.collect { hasSeen ->
+          if (!hasSeen) {
+            debugPreferences.edit { it[DebugPreferences.Keys.seenDebugDrawer] = true }
+            drawerState.open()
+          }
+        }
+      }
+
+      // Update system bar icon colors when drawer is open/closed
+      val systemUiController = rememberSystemUiController()
+      var storedStatusBarDarkContent by remember {
+        mutableStateOf(systemUiController.statusBarDarkContentEnabled)
+      }
+      var storedNavBarDarkContent by remember {
+        mutableStateOf(systemUiController.navigationBarDarkContentEnabled)
+      }
+      LaunchedEffect(systemUiController) {
+        snapshotFlow { drawerState.currentValue }
+          .collect { value ->
+            when (value) {
+              DrawerValue.Closed -> {
+                systemUiController.statusBarDarkContentEnabled = storedStatusBarDarkContent
+                systemUiController.navigationBarDarkContentEnabled = storedNavBarDarkContent
+              }
+              DrawerValue.Open -> {
+                storedStatusBarDarkContent = systemUiController.statusBarDarkContentEnabled
+                storedNavBarDarkContent = systemUiController.navigationBarDarkContentEnabled
+                systemUiController.systemBarsDarkContentEnabled = false
+              }
+            }
+          }
+      }
       ModalNavigationDrawer(
         drawerContent = {
           CompositionLocalProvider(LocalLayoutDirection provides original) {
