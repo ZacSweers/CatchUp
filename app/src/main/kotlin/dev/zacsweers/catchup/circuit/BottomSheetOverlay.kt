d8f3bf901b0baff8e2f7b957988ec663b08bd5b9
Zac Sweers <pandanomic@gmail.com>
2023-03-10 21:59:48 -0500

    Fixes for bottom sheet

--- app/src/main/kotlin/dev/zacsweers/catchup/circuit/BottomSheetOverlay.kt
+++ app/src/main/kotlin/dev/zacsweers/catchup/circuit/BottomSheetOverlay.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetValue
-import androidx.compose.material3.rememberSheetState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -34,7 +34,7 @@ class BottomSheetOverlay<Model : Any, Result : Any>(
   override fun Content(navigator: OverlayNavigator<Result>) {
     var hasShown by remember { mutableStateOf(false) }
     val sheetState =
-      rememberSheetState(
+      rememberModalBottomSheetState(
         confirmValueChange = { newValue ->
           if (hasShown && newValue == SheetValue.Hidden) {
             dismissOnTapOutside
@@ -49,7 +49,15 @@ class BottomSheetOverlay<Model : Any, Result : Any>(
       modifier = Modifier.fillMaxSize(),
       content = {
         val coroutineScope = rememberCoroutineScope()
-        BackHandler(enabled = sheetState.isVisible) { coroutineScope.launch { sheetState.hide() } }
+        BackHandler(enabled = sheetState.isVisible) {
+          coroutineScope
+            .launch { sheetState.hide() }
+            .invokeOnCompletion {
+              if (!sheetState.isVisible) {
+                navigator.finish(onDismiss!!.invoke())
+              }
+            }
+        }
         // Delay setting the result until we've finished dismissing
         content(model) { result ->
           // This is the OverlayNavigator.finish() callback
