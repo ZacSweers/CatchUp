1bb4438d2a15c6fe1b169f501a43c6552ea5ae42
Zac Sweers <pandanomic@gmail.com>
2023-03-19 20:04:12 -0400

    Remove HEAD checks for content types (#577)
    They're too slow! Just do superficial checks for now

--- app/src/main/kotlin/dev/zacsweers/catchup/service/ContentTypeChecker.kt
+++ app/src/main/kotlin/dev/zacsweers/catchup/service/ContentTypeChecker.kt
@@ -3,17 +3,12 @@ package dev.zacsweers.catchup.service
 import dev.zacsweers.catchup.di.AppScope
 import dev.zacsweers.catchup.di.SingleIn
 import io.sweers.catchup.service.api.ContentType
-import java.io.IOException
 import javax.inject.Inject
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.withContext
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
 
 @SingleIn(AppScope::class)
-class ContentTypeChecker @Inject constructor(private val okHttpClient: dagger.Lazy<OkHttpClient>) {
-  suspend fun contentType(url: String): ContentType? {
+class ContentTypeChecker @Inject constructor() {
+  fun contentType(url: String): ContentType {
     // Fast-path basic media checks
     if (url.hasMediaExtension) {
       return ContentType.IMAGE
@@ -21,20 +16,23 @@ class ContentTypeChecker @Inject constructor(private val okHttpClient: dagger.La
     if (url.isMediaUrl) {
       return ContentType.IMAGE
     }
-    val response =
-      withContext(IO) {
-        try {
-          okHttpClient.get().newCall(Request.Builder().url(url).head().build()).execute()
-        } catch (e: IOException) {
-          return@withContext null
-        }
-      }
-        ?: return null
-    return if (response.isSuccessful) {
-      response.header("Content-Type")?.contentType()
-    } else {
-      null
-    }
+
+    return ContentType.OTHER
+    // TODO the below is too slow to use tbh, need a better alternative
+    //    val response =
+    //      withContext(IO) {
+    //        try {
+    //          okHttpClient.get().newCall(Request.Builder().url(url).head().build()).execute()
+    //        } catch (e: IOException) {
+    //          return@withContext null
+    //        }
+    //      }
+    //        ?: return null
+    //    return if (response.isSuccessful) {
+    //      response.header("Content-Type")?.contentType()
+    //    } else {
+    //      null
+    //    }
   }
 }
 
