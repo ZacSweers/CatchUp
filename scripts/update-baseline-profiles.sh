096a10159fca182ab71eb21fa400ff97d4e45709
Zac Sweers <pandanomic@gmail.com>
2023-05-19 01:35:14 -0400

    Fix check

--- scripts/update-baseline-profiles.sh
+++ scripts/update-baseline-profiles.sh
@@ -3,7 +3,7 @@ set -uo pipefail
 
 # If on CI, add indirect swiftshader arg
 gpu_arg=""
-if [[ "$CI" == "true" ]]; then
+if [[ "${CI:-}" = "true" ]]; then
   gpu_arg="-Pandroid.testoptions.manageddevices.emulator.gpu=swiftshader_indirect"
 fi
 
