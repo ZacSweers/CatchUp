3207967ce71aa0b1ea14262e07622555ed62351e
Zac Sweers <pandanomic@gmail.com>
2023-06-07 16:20:54 -0400

    Enable dependency analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ plugins {
   alias(libs.plugins.retry) apply false
   alias(libs.plugins.bugsnag) apply false
   alias(libs.plugins.sortDependencies) apply false
+  alias(libs.plugins.dependencyAnalysis)
 }
 
 buildscript {
