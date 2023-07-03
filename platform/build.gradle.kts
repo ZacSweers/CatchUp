b33ce305701a5173d12a4d7541858fa5a6e0aa82
Zac Sweers <pandanomic@gmail.com>
2023-07-03 15:08:51 -0400

    Disable rake on platform

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -12,3 +12,7 @@ for (name in catalogExtension.catalogNames) {
   val catalog = catalogExtension.named(name)
   Platforms.applyFromCatalog(project, catalog)
 }
+
+// https://github.com/slackhq/slack-gradle-plugin/issues/439
+tasks.matching { it.name == "rakeDependencies" }
+  .configureEach { enabled = false }
