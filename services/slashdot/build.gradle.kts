6f59eeffc51c406b4c20085ac56dcb397fb42b29
Zac Sweers <pandanomic@gmail.com>
2023-07-03 23:15:56 -0400

    slashdot again

--- services/slashdot/build.gradle.kts
+++ services/slashdot/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
   api(libs.kotlinx.serialization.core)
   api(libs.okhttp.core)
   api(libs.retrofit.core)
+  api(libs.xmlutil.serialization)
   api(projects.libraries.appconfig)
   api(projects.libraries.di)
 
@@ -43,12 +44,6 @@ dependencies {
   implementation(libs.okhttp.core)
   implementation(libs.retrofit.kotlinxSerialization)
   implementation(libs.tikxml.htmlEscape)
-  // These are loaded at runtime via kotlinx-serialization
-  // TODO what else should DAGP do here?
-  // dependency-rake=ignore
-  implementation(libs.xmlutil.core)
-  // dependency-rake=ignore
-  implementation(libs.xmlutil.serialization)
   implementation(projects.libraries.retrofitconverters)
 
   testImplementation(libs.test.junit)
