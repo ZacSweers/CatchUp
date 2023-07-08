d829cb0bd49ff133b53f5e0224960dcba69762ae
Zac Sweers <pandanomic@gmail.com>
2023-07-08 13:26:25 -0400

    Remove workaround

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -12,7 +12,3 @@ for (name in catalogExtension.catalogNames) {
   val catalog = catalogExtension.named(name)
   Platforms.applyFromCatalog(project, catalog)
 }
-
-// https://github.com/slackhq/slack-gradle-plugin/issues/439
-tasks.matching { it.name == "rakeDependencies" }
-  .configureEach { enabled = false }
