6d096efe71d81358852973b7bc44037e4c3ef60c
Zac Sweers <pandanomic@gmail.com>
2023-08-05 17:06:03 -0400

    Posterity note

--- services/github/src/main/kotlin/io/sweers/catchup/service/github/GitHubTrendingParser.kt
+++ services/github/src/main/kotlin/io/sweers/catchup/service/github/GitHubTrendingParser.kt
@@ -40,6 +40,7 @@ internal object GitHubTrendingParser {
     // /creativetimofficial/material-dashboard
     val authorAndName =
       element.select("h1 > a").attr("href").removePrefix("/").trimEnd().split("/").let {
+        // TODO this is unreliable and fails often
         Pair(it[0], it[1])
       }
     val (author, repoName) = authorAndName
