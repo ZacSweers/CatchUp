815c2fc188d1e80166af26a109991daf58c909b5
Zac Sweers <pandanomic@gmail.com>
2023-08-14 23:28:51 -0400

    Fix missing sgp plugin in benchmark proj + regenerate (#887)

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -5,31 +5,20 @@ import com.android.build.api.dsl.ManagedVirtualDevice
 plugins {
   id("com.android.test")
   kotlin("android")
+  alias(libs.plugins.sgp.base)
   alias(libs.plugins.baselineprofile)
 }
 
-// TODO fix these in SGP
-java {
-  toolchain {
-    languageVersion.set(JavaLanguageVersion.of(libs.versions.jdk.get().toInt()))
-  }
-}
-
-tasks.withType<JavaCompile>().configureEach {
-  options.release.set(libs.versions.jvmTarget.get().toInt())
-}
+val mvdName = "pixel6Api31"
 
 android {
-  compileSdk = 34 // TODO add in SGP
   namespace = "io.sweers.catchup.benchmark"
   defaultConfig {
-    minSdk = 28 // TODO add in SGP
-    targetSdk = 34
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
   testOptions.managedDevices.devices {
-    create<ManagedVirtualDevice>("pixel6Api31") {
+    create<ManagedVirtualDevice>(mvdName) {
       device = "Pixel 6"
       apiLevel = 31
       systemImageSource = "aosp"
@@ -43,7 +32,7 @@ android {
 baselineProfile {
   // This specifies the managed devices to use that you run the tests on. The default
   // is none.
-  managedDevices += "pixel6Api31"
+  managedDevices += mvdName
 
   // This enables using connected devices to generate profiles. The default is true.
   // When using connected devices, they must be rooted or API 33 and higher.
