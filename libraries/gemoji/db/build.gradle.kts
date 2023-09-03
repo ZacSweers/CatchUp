3c7befaa29c6eb81f42571aa5284e6e57709545a
Zac Sweers <pandanomic@gmail.com>
2023-09-02 23:36:47 -0400

    Migrate jvm projects to KMP where appropriate

--- libraries/gemoji/db/build.gradle.kts
+++ libraries/gemoji/db/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
   alias(libs.plugins.kotlin.multiplatform)
   alias(libs.plugins.sgp.base)
   alias(libs.plugins.sqldelight)
+  // TODO https://github.com/slackhq/slack-gradle-plugin/issues/559
+  id("com.android.lint")
 }
 
 kotlin {
@@ -18,8 +20,8 @@ sqldelight {
   databases {
     create("GemojiDatabase") {
       packageName.set("catchup.gemoji.db")
-      schemaOutputDirectory.set(layout.projectDirectory.dir("src/main/sqldelight/databases"))
-      migrationOutputDirectory.set(layout.projectDirectory.dir("src/main/sqldelight/migrations"))
+      schemaOutputDirectory.set(layout.projectDirectory.dir("src/commonMain/sqldelight/databases"))
+      migrationOutputDirectory.set(layout.projectDirectory.dir("src/commonMain/sqldelight/migrations"))
     }
   }
 }
