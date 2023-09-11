a6fcf22fe4d5793b2ebfe3663c7272e8accacdee
Zac Sweers <pandanomic@gmail.com>
2023-09-10 22:42:47 -0400

    Scale up in wiggle too

--- libraries/compose-extensions/src/main/kotlin/catchup/compose/Wigglable.kt
+++ libraries/compose-extensions/src/main/kotlin/catchup/compose/Wigglable.kt
@@ -3,6 +3,7 @@ package catchup.compose
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -10,18 +11,32 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.graphicsLayer
 
 @Composable
-fun <T> Wigglable(target: T, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+fun <T> Wigglable(
+  target: T,
+  modifier: Modifier = Modifier,
+  content: @Composable BoxScope.() -> Unit
+) {
   var triggerWiggle by remember { mutableStateOf(false) }
 
   LaunchedEffect(target) { triggerWiggle = true }
 
-  // TODO make this better
   val angle by
     animateFloatAsState(
       targetValue = if (triggerWiggle) 10f else 0f,
+      animationSpec =
+        tween(
+          durationMillis = 200,
+        ),
+      label = "Wiggle"
+    )
+
+  val scale by
+    animateFloatAsState(
+      targetValue = if (triggerWiggle) 1.2f else 1f,
       animationSpec =
         tween(
           durationMillis = 200,
@@ -35,5 +50,8 @@ fun <T> Wigglable(target: T, modifier: Modifier = Modifier, content: @Composable
     }
   }
 
-  Box(modifier = modifier.graphicsLayer(rotationZ = angle)) { content() }
+  Box(
+    modifier = modifier.graphicsLayer(rotationZ = angle).scale(scale),
+    content = content,
+  )
 }
