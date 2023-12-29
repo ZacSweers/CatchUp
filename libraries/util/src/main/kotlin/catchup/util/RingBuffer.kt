0437813509f275c4f125281b4b498ba053accdd4
Zac Sweers <pandanomic@gmail.com>
2023-12-29 15:16:59 -0500

    Make RingBuffer concurrent

--- libraries/util/src/main/kotlin/catchup/util/RingBuffer.kt
+++ libraries/util/src/main/kotlin/catchup/util/RingBuffer.kt
@@ -1,11 +1,13 @@
 package catchup.util
 
+import java.util.concurrent.LinkedBlockingDeque
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 
-/** A simple ring buffer implementation. */
+/** A simple concurrent ring buffer implementation. */
+// TODO if this ever moves to KMP, need to find an alternative to LinkedBlockingDeque
 class RingBuffer<T>(private val capacity: Int) {
-  private val deque: ArrayDeque<T> = ArrayDeque(capacity)
+  private val deque = LinkedBlockingDeque<T>(capacity)
 
   val size: Int
     get() = deque.size
@@ -17,13 +19,7 @@ class RingBuffer<T>(private val capacity: Int) {
     deque.addLast(item)
   }
 
-  fun pop(): T? = if (deque.isNotEmpty()) deque.removeFirst() else null
-
-  fun peek(): T? = deque.firstOrNull()
-
-  fun clear() {
-    deque.clear()
-  }
+  fun clear() = deque.clear()
 
   fun toImmutableList(): ImmutableList<T> = deque.toImmutableList()
 }
