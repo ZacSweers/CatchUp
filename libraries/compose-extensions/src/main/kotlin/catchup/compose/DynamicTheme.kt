38c4e8086feacb062bc759924f15d14229aaab95
Zac Sweers <pandanomic@gmail.com>
2024-02-04 20:15:48 -0500

    Suppression

--- libraries/compose-extensions/src/main/kotlin/catchup/compose/DynamicTheme.kt
+++ libraries/compose-extensions/src/main/kotlin/catchup/compose/DynamicTheme.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.platform.LocalContext
 
 val LocalDynamicTheme = compositionLocalOf { false }
 
+@Suppress("NOTHING_TO_INLINE") // Required in K2
 @Composable
 inline fun dynamicAwareColor(
   regularColor: @Composable () -> Color,
