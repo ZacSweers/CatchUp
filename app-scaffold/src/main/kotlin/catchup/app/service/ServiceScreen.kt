723eacd5d89ad185e492984dd28b1322fd50a072
Zac Sweers <pandanomic@gmail.com>
2024-02-18 23:49:55 -0500

    Another main fix

--- app-scaffold/src/main/kotlin/catchup/app/service/ServiceScreen.kt
+++ app-scaffold/src/main/kotlin/catchup/app/service/ServiceScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
