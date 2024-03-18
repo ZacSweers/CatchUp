1fd9dae13c2e818440263dc8a71f598aa7d38959
Zac Sweers <pandanomic@gmail.com>
2024-03-18 00:11:55 -0400

    Vertically align

--- app-scaffold/src/main/kotlin/catchup/app/service/ActionRow.kt
+++ app-scaffold/src/main/kotlin/catchup/app/service/ActionRow.kt
@@ -3,16 +3,15 @@ package catchup.app.service
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.unit.dp
 import catchup.app.service.bookmarks.BookmarkIconScreen
 import com.slack.circuit.foundation.CircuitContent
 
@@ -27,6 +26,7 @@ fun ActionRow(
 ) {
   Row(
     horizontalArrangement = Arrangement.SpaceEvenly,
+    verticalAlignment = Alignment.CenterVertically,
     modifier = modifier.fillMaxWidth(),
   ) {
     val bookmarkIconScreen = remember(itemId) { BookmarkIconScreen(itemId, themeColor.toArgb()) }
