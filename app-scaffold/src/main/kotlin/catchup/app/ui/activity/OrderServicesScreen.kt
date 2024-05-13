d94a763da418ea107b850b3569dd797c22878f4f
Henry Addo <henry@addhen.org>
2024-05-13 16:01:39 -0400

    Call navigationBarsPadding on save Fab to add needed padding

--- app-scaffold/src/main/kotlin/catchup/app/ui/activity/OrderServicesScreen.kt
+++ app-scaffold/src/main/kotlin/catchup/app/ui/activity/OrderServicesScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -281,7 +282,7 @@ fun OrderServices(state: State, modifier: Modifier = Modifier) {
             Modifier.indication(
               interactionSource,
               indication = rememberRippleCompat(color = Color.White),
-            ),
+            ).navigationBarsPadding(),
           // TODO show syllabus on fab
           //  .onGloballyPositioned { coordinates ->
           //    val (x, y) = coordinates.positionInRoot()
