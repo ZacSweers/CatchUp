ceebfe3607e3f3b08e1c72f25feeabdb9913476b
Zac Sweers <pandanomic@gmail.com>
2025-07-01 17:31:04 -0400

    Fix paths

--- scripts/compute_affected_projects.sh
+++ scripts/compute_affected_projects.sh
@@ -4,9 +4,9 @@ project_root="$(git rev-parse --show-toplevel)"
 SERIALIZED_BUILD_GRAPH="${project_root}/.cache/dependencyGraph/serializedGraph.bin"
 CHANGED_FILES="tmp/changed_files.txt"
 
-./tooling/scripts/kscripts/parse-dependency-graph.main.kts --output-file "${SERIALIZED_BUILD_GRAPH}" --compute-graph --verbose
+./scripts/parse-dependency-graph.main.kts --output-file "${SERIALIZED_BUILD_GRAPH}" --compute-graph --verbose
 
-./tooling/scripts/kscripts/skippy.main.kts \
+./scripts/skippy.main.kts \
     --changed-files "${CHANGED_FILES}" \
     --dependency-graph "${SERIALIZED_BUILD_GRAPH}" \
     --outputs-dir build/skippy \
