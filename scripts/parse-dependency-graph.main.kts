a621df2238058d05e39183e72839d7cdd715c5f3
Zac Sweers <pandanomic@gmail.com>
2026-06-29 18:46:13 -0400

    Restore script shebangs

--- scripts/parse-dependency-graph.main.kts
+++ scripts/parse-dependency-graph.main.kts
@@ -1,17 +1,5 @@
-/*
- * Copyright (C) 2026. Zac Sweers
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#!/usr/bin/env kotlin
+// Gradle plugin repo required for the module-graph-assert plugin dep
 @file:Repository("https://plugins.gradle.org/m2")
 @file:DependsOn("com.slack.foundry:cli:0.35.2")
 @file:DependsOn("com.slack.foundry:skippy:0.35.2")
